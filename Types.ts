9c5305369f0583e4db9357e6fe9632372c33f01a
karthik <karthikeyavempala>
2025-04-01 12:06:03 +0530

    Types

---
+++ Types.ts
@@ -0,0 +1,21 @@
+/**!SECTION
+ * 
+ * arrays
+ * 
+ * let<variable_name>:type[]=[val1,val2]
+ * 
+ */
+
+var ids:number[]=[1,2,3,4,5];
+console.log(ids)
+
+
+//hetro
+var idd:any[]=["true",false,1,2,3]
+console.log(idd);
+
+var data:(number|string)[]=["hello",1,2,3];
+console.log(data);
+
+var data1:[number,string]=[1,"hello"];//type tuple
+
