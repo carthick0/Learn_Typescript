c661534c33b13d9b0a525bcaad6d1d8c5621ae93
karthik <karthikvempala31@gmail.com>
2025-04-02 22:51:23 +0530

    functions

---
+++ Functions.ts
@@ -0,0 +1,11 @@
+function summ():number{
+    return 10;
+}
+
+function sum(a:number,b:number){
+    return a+b;
+}
+console.log(sum(10,29))
+
+
+//how type script handle run time error ->by converting into js 
