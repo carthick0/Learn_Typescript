d870e734ae2ba6756407011d4782d8c160e41567
karthik <karthikvempala31@gmail.com>
2025-04-04 16:12:42 +0530

    class

---
+++ Class.ts
@@ -0,0 +1,17 @@
+
+class Product{
+    name:string;
+    price:any;
+    readonly category:string // by enabling readonly we caonnt change the value inside and outside of the class  . It is jst like a const variable
+
+    constructor(name:string,category:string,price:any){
+        this.name=name;
+        this.price=price;
+        this.category=category
+    }
+
+    display():void{
+        console.log("Product name",this.name,"Price is",this.price);
+
+    }
+}
